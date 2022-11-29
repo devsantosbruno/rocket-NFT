e80c1c0207377507ee9d6de7c281c9ed88fc27fa
Bruno Santos <devbrunosantos@gmail.com>
2022-11-29 12:58:18 -0300

    feat: tailwind settings

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/*.tsx", "./src/**/*.tsx", "./index.html"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
