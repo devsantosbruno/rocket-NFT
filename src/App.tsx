b8167a4c202f4fdf14cd9b54e9702506ce988559
Bruno Santos <devbrunosantos@gmail.com>
2022-11-30 21:55:18 -0300

    feat: astronaut component for desktop

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Astronaut } from "./components/Astronaut";
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import "./styles/global.css";
@@ -36,6 +37,132 @@ export function App() {
               <figure>
                 <img src="badge.svg" alt="" className="ml-auto" />
               </figure>
+            </div>
+
+            <div className="grid grid-cols-4 gap-x-4 gap-y-14 mt-24">
+              {[
+                ["gallery-1.png"],
+                ["gallery-2.png"],
+                ["gallery-3.png"],
+                ["gallery-4.png"],
+                ["gallery-5.png"],
+                ["gallery-6.png"],
+                ["gallery-7.png"],
+                ["gallery-8.png"],
+              ].map(([title]) => (
+                <img src={title} alt="" className="border-[1px] w-full" />
+              ))}
+            </div>
+          </div>
+        </section>
+
+        <section className="bg-[#0E0E0E] border-b-[1px] border-[#5A5757]">
+          <div className="container px-4 mx-auto">
+            <div className="grid grid-cols-5">
+              <div className="flex flex-col justify-evenly border-r-[1px] border-[#5A5757]">
+                <div className="flex flex-col text-center">
+                  <strong className="text-white text-5xl">10K+</strong>
+                  <span className="text-[#5A5757] font-semibold text-xl">
+                    Artes
+                  </span>
+                </div>
+
+                <div className="flex flex-col text-center">
+                  <strong className="text-white text-5xl">200+</strong>
+                  <span className="text-[#5A5757] font-semibold text-xl">
+                    Vendas
+                  </span>
+                </div>
+
+                <div className="flex flex-col text-center">
+                  <strong className="text-white text-5xl">20</strong>
+                  <span className="text-[#5A5757] font-semibold text-xl">
+                    Artistas
+                  </span>
+                </div>
+              </div>
+
+              <div className="col-span-4">
+                <div className="flex flex-col justify-between h-full">
+                  <div className="flex gap-x-32 py-16 px-16">
+                    <div className="text-white text-[1.125rem]">
+                      <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Congue malesuada amet pretium lacus. Id
+                        <a href="#" className="text-[#FF5B50]">
+                          {" "}
+                          potenti diam
+                        </a>{" "}
+                        et cras odio viverra. Amet, velit ut hac sit. Enim
+                        ultricies mauris mattis nunc semper.
+                      </p>
+
+                      <div className="mt-16">
+                        <a
+                          href="#"
+                          className="underline text-white hover:text-[#FF5B50] transition duration-200"
+                        >
+                          Lorem ipsum
+                        </a>
+                      </div>
+                    </div>
+
+                    <div className="text-white text-[1.125rem]">
+                      <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Congue malesuada amet pretium lacus. Id
+                        <a href="#" className="text-[#FF5B50]">
+                          {" "}
+                          potenti diam
+                        </a>{" "}
+                        et cras odio viverra. Amet, velit ut hac sit. Enim
+                        ultricies mauris mattis nunc semper.
+                      </p>
+
+                      <div className="mt-16">
+                        <a
+                          href="#"
+                          className="underline text-white hover:text-[#FF5B50] transition duration-200"
+                        >
+                          Lorem ipsum
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+
+                  <img
+                    src="banner2.png"
+                    alt=""
+                    className="w-full h-full object-cover object-center"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="bg-[#0E0E0E] py-36">
+          <div className="container px-4 mx-auto">
+            <h2 className="text-white text-5xl font-bold">
+              <span className="text-[#FF5B50]">Populares</span> da semana
+            </h2>
+
+            <div className="grid grid-cols-5 mt-40">
+              <div className="flex flex-col gap-20">
+                <Astronaut astronaut={1} name="Mayk Brito" rkt={1.5} />
+
+                <Astronaut astronaut={3} name="João Inácio (Biro)" rkt={3.75} />
+              </div>
+
+              <div className="h-full col-span-3">
+                <div className="h-full w-[1px] bg-[#5A5757] mx-auto"></div>
+              </div>
+
+              <div className="flex flex-col gap-20">
+                <Astronaut astronaut={2} name="Jakeliny Gracielly" rkt={2.0} />
+
+                <Astronaut astronaut={4} name="Tamires Santos" rkt={4.3} />
+              </div>
             </div>
           </div>
         </section>
---
+++ src/components/Astronaut.tsx
@@ -0,0 +1,23 @@
+import { ArrowUpRight } from "phosphor-react";
+
+export function Astronaut(props: any) {
+  return (
+    <div>
+      <div className="flex items-center justify-between text-white text-3xl">
+        <h3>Astronauta {props.astronaut}</h3>
+        <ArrowUpRight size={24} />
+      </div>
+
+      <div className="flex items-center justify-between text-xl mt-10 mb-6">
+        <h3 className="text-[#5A5757]">{props.name}</h3>
+        <span className="text-[#FF5B50]">{props.rkt} RKT</span>
+      </div>
+
+      <img
+        src={`astronaut-${props.astronaut}.png`}
+        alt=""
+        className="w-full h-full max-h-48 object-cover object-center"
+      />
+    </div>
+  );
+}
