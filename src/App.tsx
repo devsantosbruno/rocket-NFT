50a4f6bfb0e81be924f5595341cd94d75a3198e5
Bruno Santos <devbrunosantos@gmail.com>
2022-12-04 17:50:49 -0300

    feat: component with Header

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Artist } from "./components/Artist";
 import { Astronaut } from "./components/Astronaut";
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
+
 import "./styles/global.css";
 
 export function App() {
